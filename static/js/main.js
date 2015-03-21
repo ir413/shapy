91111bc2c068c27a03cdeb014f04fed6bc447365
Radu Szasz <ras114@ic.ac.uk>
2015-03-21 14:38:12 +0000

    Got login directive working

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,13 @@
 
 
 
-var shapy = angular.module('shapy', []);
+angular.module('shapy', [])
+  .directive('shapyLogin', function() {
+    return {
+      restrict: 'E',
+      link: function($scope, $elem, $attrs) {
+        console.log('link');
+      }
+    };
+  });
 
